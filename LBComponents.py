3795e1137c9a08fbdf00eae17495e6accf6bdd13
F_Rosa <fco.rosa@yahoo.com.br>
2025-12-07 10:06:09 -0300

    Rename function get_values_continuos to get_continuos_values

--- LBComponents.py
+++ LBComponents.py
@@ -78,11 +78,11 @@ def get_sky_matrix_values(epw_path = "",
         #get continuos irradiance values
         location = epw.location
         #update timestep > 1
-        direct_normal_irradiance = get_values_continuos(header = header,
+        direct_normal_irradiance = get_continuos_values(header = header,
                                                         values = dnr_values,
                                                         timestep = timestep
                                                         )
-        diffuse_horizontal_irradiance = get_values_continuos(header = header,
+        diffuse_horizontal_irradiance = get_continuos_values(header = header,
                                                         values = dhr_values,
                                                         timestep =  timestep
                                                         )
@@ -827,7 +827,7 @@ def get_header(data_type=None,
 # 9. HourlyContinuousCollection
 #=================================================
 
-def get_values_continuos(header = None,
+def get_continuos_values(header = None,
                          values = None,
                          timestep = None
                          ):
